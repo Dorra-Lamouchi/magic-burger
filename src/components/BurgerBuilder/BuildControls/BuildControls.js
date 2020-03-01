7f33f774bda8eda64384b15db747eb9a0d3c0d00
Dorra <lamouchidorra@gmail.com>
2020-03-01 14:26:05 +0100

    build control

--- src/components/BurgerBuilder/BuildControls/BuildControls.js
+++ src/components/BurgerBuilder/BuildControls/BuildControls.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import classes from './BuildControls.module.css'
+import BuildControl from './BuildControl/BuildControl'
 
-const buildControls = () => {
+
+const buildControls = (props) => {
+
+    const ingredients = props.ingredients.map((ingredient) => {
+        return <BuildControl
+            label={ingredient.label}
+            added={props.added}
+            removed={props.removed}
+            key={ingredient.id}
+        />
+    })
+
     return (
-        <div>
-            build controls
+        <div className={classes.BuildControls}>
+            <p>Current Price: 4</p>
+            {ingredients}
+            <button className={classes.OrderButton}>ORDER NOW</button>
+
         </div>
     )
 }
--- src/components/BurgerBuilder/BurgerBuilder.js
+++ src/components/BurgerBuilder/BurgerBuilder.js
@@ -2,13 +2,88 @@ import React, { Component } from 'react'
 import Burger from './Burger/Burger'
 import BuildControls from './BuildControls/BuildControls'
 
+
 class BurgerBuilder extends Component {
 
+    state = {
+        ingredients: [
+            {
+                id: "s",
+                label: "salad",
+                count: 0
+            },
+            {
+                id: "m",
+                label: "meat",
+                count: 0
+            },
+            {
+                id: "c",
+                label: "cheese",
+                count: 0
+            },
+            {
+                id: "e",
+                label: "escalope",
+                count: 0
+            }
+        ]
+    }
+    addIngredientHandler = (ingredient) => {
+
+        const newIngredients = [...this.state.ingredients]
+
+        const i = newIngredients.indexOf(ingredient)
+
+        newIngredients[i].count = newIngredients[i].count + 1
+
+        this.setState({
+            ingredients: newIngredients
+        })
+    }
+
+    removeIngredientHandler = (id) => {
+
+        const newIngredients = [...this.state.ingredients]
+
+        const index = newIngredients.findIndex((ingredient) => {
+            return ingredient.id === id
+
+        })
+        //const index = newIngredients.indexOf(id)
+
+
+        if (newIngredients[index].count > 0)
+            newIngredients[index].count = newIngredients[index].count - 1
+
+        this.setState({
+            ingredients: newIngredients
+        })
+    }
+
+
     render() {
+        let buildControls = null
+        buildControls = (<div>
+            {
+                this.state.ingredients.map((ingredient) => {
+
+                    return (
+                        <BuildControls
+                            ingredients={this.state.ingredients}
+                            added={() => { this.addIngredientHandler(ingredient) }}
+                            removed={() => { this.removeIngredientHandler(ingredient) }}
+                            key={ingredient.id}
+                        />
+                    )
+                })}
+        </div>)
         return (
             <div>
-                <Burger />
-                <BuildControls />
+                <Burger
+                    ingredients={this.state.ingredients}
+                />
+                {buildControls}
             </div>
         )
     }
